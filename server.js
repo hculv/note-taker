338675b8e014d10dcc7a45067c10d97e97f6a92c
Hannah CZ <hannahreedculver@gmail.com>
2022-03-19 19:52:28 -0400

    updates

--- server.js
+++ server.js
@@ -3,20 +3,15 @@ const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
-
-// Middleware
 app.use('/api', require('./routes/apiRoutes.js'));
 
 app.use(express.static('public'));
-
-// Routes
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/Public/notes.html'));
 })
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Publicublic/index.html'))
+    res.sendFile(path.join(__dirname, 'Public/index.html'))
 })
 
-// Listening
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
