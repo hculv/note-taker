ed7519bab348e3c8266a59254a069b411f9c029a
Hannah CZ <hannahreedculver@gmail.com>
2022-05-21 15:57:38 -0400

    new .js file

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
+// update
+
 const express = require('express');
 const app = express();
 const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
 app.use('/api', require('./routes/apiRoutes.js'));
+
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/Public/notes.html'));
-});
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Public/index.html'))
-});
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+})
 
-app.listen(PORT, () => 
-console.log(`Application is live on ${PORT}`)
-);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+app.listen(PORT, () => console.log(`Live on port: ${PORT}`));
